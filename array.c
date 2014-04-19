1f58b7c04e08a3ec8d5151beb22c5e23b8040495
aviram <aviram@emca>
2014-04-20 02:21:37 +0300

    Added max related functions.

--- array.c
+++ array.c
@@ -39,3 +39,27 @@ bool array__read_from_file(char * const filename, unsigned int array_size, int a
 
     return true;
 }
+
+unsigned int array__find_max_index(int array[], unsigned int start_index, unsigned int end_index)
+{
+    int current_max_index = start_index;
+    unsigned int i = start_index;
+
+    for (i = start_index; i <= end_index; i++) {
+        if (array[i] > array[current_max_index]) {
+            current_max_index = i;
+        }
+    }
+
+    return current_max_index;
+}
+
+void array__max_to_first(int array[], unsigned int start_index, unsigned int end_index)
+{
+    int temp = 0;
+    int max_index = array__find_max_index(array, start_index, end_index);
+
+    temp = array[max_index];
+    array[max_index] = array[0];
+    array[0] = temp;
+}
