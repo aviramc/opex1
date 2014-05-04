4b15437a54b50a6745e6dbcc463477daec129d71
aviram <aviram@adallom.com>
2014-05-04 08:04:43 +0300

    Removed TODOs

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-# TODO: Makefile, gcc flags.
 gcc -g -Wall -std=gnu99 number.c array.c main.c -o ex14
--- build_test.sh
+++ build_test.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# TODO: Makefile, gcc flags.
 gcc -g -Wall -std=gnu99 number.c array.c test_array.c -o test_array
 
 gcc -g -Wall -std=gnu99 number.c test_number.c -o test_number
